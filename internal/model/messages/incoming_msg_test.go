47200960b2aff94670a191fb509e5c88dcdd03a6
Alexei Kornev <alex1234562557@gmail.com>
2022-09-30 01:37:06 +0700

    [GOHW-1] add mock test

---
+++ internal/model/messages/incoming_msg_test.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	mocks "gitlab.ozon.dev/alex1234562557/telegram-bot/internal/mocks/messages"
+)
+
+func Test_OnStartCommand_ShouldAnswerWithIntroMessage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	sender := mocks.NewMockMessageSender(ctrl)
+	model := New(sender)
+
+	sender.EXPECT().SendMessage("dont know this command", int64(123))
+
+	err := model.IncomingMessage(Message{
+		Text:   "some text",
+		UserID: 123,
+	})
+
+	assert.NoError(t, err)
+}

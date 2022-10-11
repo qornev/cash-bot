67ce87386c77194b73d9866f109e619fe4d9cd02
Alexei Kornev <alex1234562557@gmail.com>
2022-10-11 20:45:50 +0700

    [GOHW-2] make storage thread safe

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,37 +1,49 @@
 package storage
 
 import (
+	"sync"
+
 	"gitlab.ozon.dev/alex1234562557/telegram-bot/internal/converter"
 	"gitlab.ozon.dev/alex1234562557/telegram-bot/internal/model/messages"
 )
 
 type Storage struct {
+	mutex *sync.Mutex
 	data  map[int64][]*messages.Expense
 	state map[int64]string
 }
 
 func New() (*Storage, error) {
 	return &Storage{
+		mutex: &sync.Mutex{},
 		data:  make(map[int64][]*messages.Expense),
 		state: make(map[int64]string),
 	}, nil
 }
 
 func (s *Storage) Add(userID int64, expense *messages.Expense) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.data[userID] = append(s.data[userID], expense)
 	return nil
 }
 
 func (s *Storage) Get(userID int64) ([]*messages.Expense, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	return s.data[userID], nil
 }
 
 func (s *Storage) SetState(userID int64, currency string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.state[userID] = currency
 	return nil
 }
 
 func (s *Storage) GetState(userID int64) (string, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	if val, ok := s.state[userID]; ok {
 		return val, nil
 	}

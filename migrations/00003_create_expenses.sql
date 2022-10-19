b22e190e5c65da5f1bed3ba74b16f2de1a237f04
Alexei Kornev <alex1234562557@gmail.com>
2022-10-20 03:40:06 +0700

    [GOHW-3] add index and test data for expenses

--- migrations/00003_create_expenses.sql
+++ migrations/00003_create_expenses.sql
@@ -10,10 +10,16 @@ create table expenses
 );
 alter table expenses
     add constraint fk_expenses_users foreign key (user_id) references users(id);
+create unique index expenses_idx on expenses (dt, user_id);
+-- Создаю индекс по колонкам dt и user_id, потому что в запросах по ним происходит основное сравнение строк друг с другом
+-- B-tree, потому что оно дает быстрый поиск в глубину, т.к. оно смотрит есть ли в текущем предке нужное значение
+-- Если значение есть, то спускаемся вниз, если нет - смотрим по другим потомкам
+-- Так же можно итерироваться между листьев
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+drop index expenses_idx;
 alter table expenses 
     drop constraint fk_expenses_users;
 drop table expenses;
---
+++ migrations/00004_test_expenses_data.sql
@@ -0,0 +1,28 @@
+-- +goose Up
+-- +goose StatementBegin
+insert into expenses(dt, user_id, amount, category)
+values (1666186334, 1234, 123.45, 'еда');
+insert into expenses(dt, user_id, amount, category)
+values (1665186334, 1234, 800.00, 'интернет');
+insert into expenses(dt, user_id, amount, category)
+values (1664186334, 4321, 1500.31, 'жкх');
+insert into expenses(dt, user_id, amount, category)
+values (1663186334, 4321, 700.11, 'кино');
+insert into expenses(dt, user_id, amount, category)
+values (1662186334, 102938, 432.45, 'шампунь');
+insert into expenses(dt, user_id, amount, category)
+values (1661186334, 47389, 500.00, 'потерял');
+insert into expenses(dt, user_id, amount, category)
+values (1659186334, 213900, 780.33, 'лекарства');
+insert into expenses(dt, user_id, amount, category)
+values (1658186334, 3452, 1500.00, 'врач');
+insert into expenses(dt, user_id, amount, category)
+values (1659186334, 1234, 345.45, 'еда');
+insert into expenses(dt, user_id, amount, category)
+values (1662186334, 1234, 1.45, 'пакет');
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+truncate table expenses;
+-- +goose StatementEnd
